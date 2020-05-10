c54296c06e335b63787527e4c08fc2aee5c892c9
Mohamadelattar <mohamadelattar99@gmail.com>
2020-05-10 02:59:13 +0000

    La creation de la page header

---
+++ includes/header.php
@@ -0,0 +1,81 @@
+<?php
+
+
+?>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/fontawesome.css">
+        <link rel="stylesheet" href="css/all.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/fontawesome.js"></script>
+        <script src="js/all.js"></script>
+    </head>
+    <body>
+        <header class="container-fluid">
+            <div class="row Panel">
+                <div class="col-xs-12 col-lg-3 col-sm-4 col-md-4 title"  >
+                    <h1>SAM Shopping</h1>
+                </div>
+                
+                <div class="col-xs-12 col-lg-4 col-sm-4 col-md-5 list_header" >
+                
+                        <nav  class="navbar"   >
+                        <button class="navbar-toggler collapsed"  id="button_nav" type="button" data-toggle="collapse" data-target="#CollapseNavbar " 
+          aria-expanded="false">
+        		        	&#9776;
+                        </button>
+                <div class="collapse navbar-toggleable-xs  " id="CollapseNavbar">
+                            <ul>
+                                <li>
+                                     <div class="button_header">
+                                         
+                                        <a href="#">categories  </a>
+                                     </div>
+                                </li>
+                                
+                                <li>
+                                     <div class="button_header">
+                                        
+                                       <a href="#">about</a>
+                                    </div>
+                                </li>
+                                
+                                <li>
+                                    <div class="button_header">
+                                         
+                                        <a href="#">contact</a>
+                                     </div>
+                                </li>
+                            </ul>
+                            </div>
+                        </nav>
+                        
+                </div>
+                 
+                <div class="col-xs-12 col-lg-3 col-sm-4 col-md-3 nav_header">
+                        <nav>
+                            <ul>
+                                <li><i class="fas fa-search"></i></li>
+                                <li><i class="far fa-user"></i></li>
+                                <li><i class="far fa-heart"></i></li>
+                                <li><i class="fas fa-shopping-bag"></i></li>
+                            </ul>
+                        </nav>
+                </div>
+            </div>
+                
+        </header>
+        <div class="ligne "></div>
+    </body>
+
+
+
+
+</html>
