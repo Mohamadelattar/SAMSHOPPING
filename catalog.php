62f28ff8b49fdc49baa78d08507493243bbd62b2
Mohamadelattar <mohamadelattar99@gmail.com>
2020-05-12 00:59:37 +0000

    Little Modification

--- catalog.php
+++ catalog.php
@@ -84,7 +84,31 @@ include("includes/header.php");
             20
             </div>
         </div>
+        <div> <a href="#" id="button_items">view more</a></div>
 </section>
+<!-- Delivrey Section -->
+<section class="container-fluid" id="delivrey">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <h4>Free Delivrey</h4>
+                    
+
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <h4>Easy Return</h4>
+
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <h4>Wide choice</h4>
+
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+
+                </div>
+            </div>
+        </div> 
+    </section>
 <?php 
     include("includes/footer.php");
 ?>
