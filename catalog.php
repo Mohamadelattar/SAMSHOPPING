80f8c656e1ca81b4ae6ec439d7547d1cfc705e32
Mohamadelattar <mohamadelattar99@gmail.com>
2020-05-12 00:54:20 +0000

    La creation de la page catalogue

---
+++ catalog.php
@@ -0,0 +1,90 @@
+<?php
+include("includes/header.php");
+?>
+ 
+<section class="container-fluid" id="catalog">
+       <p><a href="#">Home</a>/<a href="#">About</a></p>
+        <br>
+        <h2>Sam Shopping</h2>
+        <div class="row">
+            <div class="col-xs-12 col-lg-3" style="text-align:left; font-family: Quicksand-VariableFont_wght , sans-serif;" >
+                <p>931,423 results in all product</p>
+            </div>
+            <div class="col-xs-12   col-md-6 col-sm-6" style="text-align:center;">
+                <div class="buttons">
+                    <button class="btn btn-danger">New</button>
+                    <button class="btn btn-danger">Popular</button>
+                    <button class="btn btn-danger">All</button>
+                    <button class="btn btn-danger">Sales</button>
+                </div>
+            </div>
+            <div class="col-xs-12 col-lg-3 col-md-6 col-sm-6" style="text-align:right;">
+                <p>Infos</p>
+            </div>
+        </div>
+        <div class="row" style="margin-top:2em;">
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            1
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            2
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            3
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            4
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            5
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            6
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            7
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            8
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            9
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            10
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            11
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            12
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            13
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            14
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            15
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            16
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            17
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            18
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            19
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            20
+            </div>
+        </div>
+</section>
+<?php 
+    include("includes/footer.php");
+?>
