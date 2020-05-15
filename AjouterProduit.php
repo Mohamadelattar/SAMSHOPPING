52a42a4d7473ef08706073bf0651dee5d423266f
Mohamadelattar <mohamadelattar99@gmail.com>
2020-05-15 00:35:46 +0000

    La creation de la page Ajouter un produit

---
+++ AjouterProduit.php
@@ -0,0 +1,44 @@
+<?php
+include("includes/header.php");
+?>
+<section class="container-fluid" id="ajouter">
+<p><a href="#">Home</a>/<a href="#">Contact</a></p>
+         
+        <h2>Ajouter un produit</h2>
+<div class="container" id="ajouterProduit" >
+    <div class="row">
+    <form action="" method="POST" class="col-lg-12">
+        <div class="ajoutElement"  >
+        <label for="title" class="col-lg-6 " >Titre : </label>
+        <input type="text" name="title" placeholder="Titre de le produit" class="col-lg-6">
+        </div>
+        <div class="ajoutElement">
+        <label for="categorie"   class="col-lg-6">Categorie : </label>
+        <select  name="categorie" class="col-lg-6">
+            <option value="smartphone">Smartphone</option>
+            <option value="pc">Pc Portable</option>
+            <option value="tablette">Tablette</option>
+            <option value="accessesoire">Accessesoire</option>
+        </select>
+        </div>
+        <div class="ajoutElement">
+        <label for="title" class="col-lg-6">Prix : </label>
+        <span>(en DH)</span>
+        <input type="text" name="title" placeholder="Prix de le produit" class="col-lg-6">
+        </div>
+        <div class="ajoutElement">
+        <label for="description" class="col-lg-6" >Description :</label><br>
+        <textarea name="description" id="" cols="" rows="10%" class="col-lg-12" ></textarea>
+        </div> 
+        <div class="ajoutElement">      
+        <label for="imageProduit" class="col-lg-6">Telecharger L'image du produit :</label>
+        <input type="file"  name="imageProduit" class="col-lg-6 " id="buttUpload">
+        </div> 
+        <input type="submit" value="Enregistrer">
+    </form>
+</div>
+</div>
+</section>
+<?php 
+    include("includes/footer.php");
+    ?>
