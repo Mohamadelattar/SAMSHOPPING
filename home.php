7e41229d9adb30d489484abda6c5d5b6df8bd17e
soulaymane <soulaymanegouijane@gmail.com>
2020-06-07 09:15:27 +0100

    new header added (just normal one )

--- home.php
+++ home.php
@@ -1,5 +1,4 @@
 <?php
- 
 require 'config/config.php';
 if(isset($_SESSION['nom']) )
 {
@@ -10,6 +9,7 @@ else
     include("includes/header.php");
 }
 
+
 ?>
 
     <!-- Background Section -->
@@ -18,7 +18,7 @@ else
         <div>
             <h2>Sam Shopping</h2>
         </div>
-    
+
     </section>
     <!-- Items Section -->
     <section class="container-fluid" id="items">
@@ -36,30 +36,30 @@ else
         <div class="container" id="new_items">
             <h3>New Items</h3>
             <div class="d-flex justify-content-center d-md-inline-flex  d-sm-inline-flex align-content-end align-content-center flex-wrap">
-            
+
             <?php
-        
+
         $result = mysqli_query($con,"SELECT * FROM `article` ORDER BY id_article DESC LIMIT 8");
         $item = "Item";
         $increment = 1;
           while( $row =  mysqli_fetch_array($result))
         {
-            $id = $item.$increment;   
+            $id = $item.$increment;
             echo "<div class='border border-primary new_item' id='" .$id . "' >" ;
             echo "<a href='#'>";
             echo ' <img  height="auto" width="100%" src="data:image/jpg;base64,' .  base64_encode($row[5])  . '" />';
-            echo "<br>"; 
+            echo "<br>";
             echo "<div class='titre_item'><p> ".$row[1]."</p>";
             echo "<span>".$row[3]."DH"."</span></div>";
             echo "</a></div>";
             $increment++;
-           
+
         }
 
       ?>
 
-         
-              
+
+
             </div>
         </div>
         <div> <a href="catalog.php" id="button_items">view more</a></div>
@@ -70,7 +70,7 @@ else
             <div class="row">
                 <div class="col-lg-4 col-md-4 col-sm-6">
                     <h4>Free Delivrey</h4>
-                    
+
 
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
@@ -85,9 +85,9 @@ else
 
                 </div>
             </div>
-        </div> 
+        </div>
     </section>
-    <?php 
+    <?php
     include("includes/cart.php");
     include("includes/footer.php");
     ?>
