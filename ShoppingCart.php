7a43d044d4855c78030f10565caf76889229a938
soulaymane <soulaymanegouijane@gmail.com>
2020-07-19 00:51:27 +0100

    problem in shopping cart

--- ShoppingCart.php
+++ ShoppingCart.php
@@ -7,30 +7,39 @@
   <div class="cart-content">
     <!-- cart-item -->
      <?php 
-     if(!empty($_SESSION["ShoppingCart"])){
-         $total=0;
-         foreach ($_SESSION["ShoppingCart"] as $key => $value) {
-     ?>
-     <div class="cart-item">
-     <img src="data:image/jpg;base64, <?php echo base64_encode($value["item_image"]) ?>" alt="test"> 
-     <div> 
-         <h4><?php echo $value["item_name"] ?></h4>
-         <h5><?php echo $value["item_price"] ?></h5>
-         <span class="remove-item">Supprimer</span>
-        </div>
-    <div>
-        <i class="fas fa-chevron-up"></i>
-        <p class="item-amount">1</p>
-        <i class="fas fa-chevron-down"></i>
+      $result = mysqli_query($con,"SELECT * FROM `ligne`;");
+        while( $row =  mysqli_fetch_array($result))
+      {
+        $sql = mysqli_query($con,"SELECT * FROM 'article' WHERE id_article=$row[1]");
+        while ($col = mysqli_fetch_array($sql) ) {
+        echo "<div class='cart-item'>";
+        echo "<img src='data:image/jpg;base64, base64_encode($col[5]) ' alt='image Produit'>";
+        echo "
+        <div> 
+        <h4> $col[1]</h4>
+        <h5> $col[3]</h5>
+        <span class='remove-item'>Supprimer</span>
+       </div>
+   <div>
+       <i class='fas fa-chevron-up'></i>
+       <p class='item-amount'>1</p>
+       <i class='fas fa-chevron-down'></i>
+   </div>
     </div>
-     </div>
+        ";
 
+        }
+
+      }
+   
+     ?>
+     
     <!-- end of cart-item -->
-  <div class="   cart-footer">
+  <div class="cart-footer">
     <h3>Le Montant Total : $ <span class="cart-total"></span></h3>
   <button class="clear-cart banner-btn btn mt-5 btn-custom btn-block text-uppercase rounded-pill btn-lg">clear cart</button>
   </div>
 </div>
          </div>
-         <?php }} ?>
+
          </div>
